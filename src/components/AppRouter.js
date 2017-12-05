639d927a7713c1b5367bfa3d7beebf0dc7b60704
hantishoy <hanchuang@angonsoft.com>
2017-12-05 13:18:26 +0800

    去掉机构部分

--- src/components/AppRouter.js
+++ src/components/AppRouter.js
@@ -19,11 +19,6 @@ import Students from '../pages/com/students/students.page.js';
 import Exams from '../pages/com/exams/exams.page.js';
 import Infos from '../pages/com/infos/info.page.js';
 
-import OrganizationHome from '../pages/org/home';
-import Area from '../pages/org/area';
-import Score from '../pages/org/score';
-import Clazz from '../pages/org/clazz';
-import Student from '../pages/org/student';
 
 import Lang from '../language';
 
@@ -71,39 +66,6 @@ var AppRouter = {
       /> */}
     </Route>
 
-  </Router>),
-  2: (<Router history={browserHistory} render={applyRouterMiddleware(useScroll())}>
-    <Route title="Training" path="/" component={AppFrame}>
-      <IndexRoute dockDrawer title={titleize(Lang[window.Lang].pages.org.home.title)} nav component={OrganizationHome} />
-      <Route
-        title={titleize(Lang[window.Lang].pages.org.home.title)}
-        path={'/org/home'}
-        content={OrganizationHome}
-        nav
-        component={OrganizationHome}
-      />
-      <Route
-        title={titleize(Lang[window.Lang].pages.org.student.title)}
-        path={'/org/student'}
-        content={Student}
-        nav
-        component={Student}
-      />
-      <Route
-        title={titleize(Lang[window.Lang].pages.org.clazz.title)}
-        path={'/org/clazz'}
-        content={Clazz}
-        nav
-        component={Clazz}
-      />
-      <Route
-        title={titleize(Lang[window.Lang].pages.org.area.title)}
-        path={'/org/area'}
-        content={Area}
-        nav
-        component={Area}
-      />
-    </Route>
   </Router>)
 }
 
